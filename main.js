de87a7782ccc6212870a563d73e295c2523280c8
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2022-10-11 18:47:34 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -20,40 +20,51 @@ if(day==="tuesday" || day==="thursday"){
     document.querySelector('body').style.backgroundColor = 'lightgreen'
     document.querySelector('#weeknd').classList.add('hidden')
     document.querySelector('#heartGoHere').classList.add('hidden')
+    document.querySelector('#oGoHere').classList.add('hidden')
     document.querySelector('body').style.color="black"
+    document.querySelector('span').style.fontStyle="normal"
 
 
 }else if (day==="saturday" || day==="sunday"){
     document.querySelector('#alertGoHere').innerText = 'WEEKEND BABY!!'
     document.querySelector('body').style.backgroundColor = "rgba(255, 255, 0, .60)"
     document.querySelector('body').style.color="black"
-    document.querySelector('#weeknd').classList.add('hidden')
-    //document.querySelector('#heartGoHere').classList.add('hidden')
+    //document.querySelector('#weeknd').classList.add('hidden')
+    document.querySelector('#oGoHere').classList.add('hidden')
+    document.querySelector('#heartGoHere').classList.add('hidden')
+    document.querySelector('span').style.fontStyle="normal"
 
 }else if(day==="wednesday"){
     document.querySelector('#alertGoHere').innerText ="hump day."
     document.querySelector('body').style.backgroundColor="pink"
     document.querySelector('#weeknd').classList.add('hidden')
     document.querySelector('#heartGoHere').classList.add('hidden')
+    document.querySelector('#oGoHere').classList.add('hidden')
     document.querySelector('body').style.color="black"
+    document.querySelector('span').style.fontStyle="normal"
 
 
 }else if (day==="friday" && hh>=17 /*&& today.getDay()===5*/){
-    document.querySelector('#alertGoHere').innerText = "XO"
+    document.querySelector('#alertGoHere').innerText = "X"
+    document.querySelector('#oGoHere').innerText = "O"
     document.querySelector('#heartGoHere').classList.remove('hidden')
+    document.querySelector('#oGoHere').classList.remove('hidden')
     document.querySelector('#weeknd').classList.remove('hidden')
     document.querySelector('#weeknd').src = "https://www.youtube.com/embed/V_cnK8Cd6Ag?autoplay=1"
     document.querySelector('body').style.backgroundColor="black"
     document.querySelector('body').style.color="white"
+    document.querySelector('span').style.fontStyle="italic"
 
-}else if (day==="monday" || day==="friday"){
+}else if (day==="monday" || day==="friday" && hh<17){
     document.querySelector('#alertGoHere').innerText = 'booooooooorriiinngggggggg!'
     document.querySelector('body').style.backgroundColor = "lightgray";
     document.querySelector('#weeknd').classList.add('hidden')
     document.querySelector('#heartGoHere').classList.add('hidden')
+    document.querySelector('#oGoHere').classList.add('hidden')
     document.querySelector('body').style.color="black"
+    document.querySelector('span').style.fontStyle="normal"
 
-}else {
+}else{
     alert('Try again');
 }
 }
